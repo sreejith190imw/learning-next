46e6ba9db7433fa96fac2a328f0b731a9082f591
sreejith190imw <sreejith@imagemediaworld.com>
2024-02-02 16:36:34 +0530

    remove nextui from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
@@ -16,6 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [],
 };
 export default config;
